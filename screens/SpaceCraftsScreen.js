e2310d4a66e28bb08efbf4df157c2d33c225bdab
kshitij1127 <gudlakshitij.1127@gmail.com>
2021-10-17 19:13:09 +0530

    finally!

--- screens/SpaceCraftsScreen.js
+++ screens/SpaceCraftsScreen.js
@@ -1,89 +1,120 @@
-import axios from "axios";
-import * as React from "react";
-import { View, Text, StyleSheet, Image, FlatList } from "react-native";
+import React, { Component } from 'react';
+import { Text, View, Alert, FlatList, Image, ImageBackground, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
 
-export default class SpaceCraftsScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      aircrafts: [],
+import axios from 'axios';
+
+export default class SpaceCraftsScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            aircrafts: []
+        };
     }
-  }
 
-  getData = () => {
-    axios.get("https://ll.thespacedevs.com/2.0.0/config/spacecraft/")
-      .then(response => {
-        this.setState({
-          aircrafts: response.data.results,
-        })
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+    componentDidMount() {
+        this.getData()
 
-  renderItem = ({ item }) => {
-    return (
-      <View style={styles.renderContainer}>
-        <Image source={{uri: item.agency.image_url}} style={styles.image}></Image>
+    }
 
-        <Text style={styles.sectionHeader}>{item.name}</Text>
-        <Text style={{color: '#696969'}}>{item.agency.name}</Text>
-        <Text>DESCRIPTION</Text>
-        <Text style={styles.descriptionText}>{item.agency.description}</Text> 
-      </View>
-    )
-  }
+    getData = () => {
+        axios.get("https://ll.thespacedevs.com/2.0.0/config/spacecraft/")
+            .then(response => {
 
-  keyExtractor = (item, index) => {
-    index.toString();
-  }
+                this.setState({ aircrafts: response.data.results })
 
-  render() {
-    return(
-      <View style={styles.container}>
-        <View style={{flex: 0.25}}>
-          <Text>Spacecrafts</Text>
-        </View>
-        <View style={{flex: 0.75}}>
-        <FlatList keyExtractor={this.keyExtractor} data={this.state.aircrafts} renderItem={this.renderItem}/>
-        </View>
-      </View>
-    )
-  }
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+
+    }
+
+
+
+
+    renderItem = ({ item }) => {
+        return (
+            <View style={styles.contentCard}>
+                <Image source={{ uri: item.agency.image_url }} style={styles.itemImage}></Image>
+                <View style={{ padding: 20 }}>
+                    <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'purple' }}>{item.name}</Text>
+                    <Text style={{ color: '#696969', fontSize: 16 }}>{item.agency.name}</Text>
+                    <View style={{ marginTop: 10 }}>
+                        <Text style={{ color: '#A9A9A9', fontSize: 13 }}>{item.agency.description}</Text>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+
+    keyExtractor = (item, index) => index.toString();
+
+    render() {
+        if (Object.keys(this.state.aircrafts).length === 0) {
+            return (
+                <View
+                    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                    <Text>Loading</Text>
+                </View>
+            )
+        } else {
+            return (
+                <View style={styles.container}>
+                    <SafeAreaView style={styles.droidSafeArea} />
+                    <ImageBackground source={require('../assets/stars.gif')} style={styles.backgroundImage}>
+                        <View style={{ flex: 0.15, alignItems: 'center', justifyContent: 'center' }}>
+                            <Text style={styles.titleText}>Spacecrafts</Text>
+                        </View>
+                        <View style={{ flex: 0.85 }}>
+                            <FlatList
+                                keyExtractor={this.keyExtractor}
+                                data={this.state.aircrafts}
+                                renderItem={this.renderItem}
+                                initialNumToRender={10}
+                            />
+                        </View>
+
+
+
+                    </ImageBackground>
+                </View>
+            )
+        }
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
-  },
-
-  renderContainer: {
-    borderWidth: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 10,
-    elevation: 10,
-  },
-
-  image: {
-    width: "100%",
-    height: 200,
-    marginTop: 15,
-    marginBottom: 15,
-    marginRight: 10,
-  },
-
-  sectionHeader: {
-    fontWeight: 'bold',
-    fontSize: '20'
-  },
-
-  descriptionText: {
-    color: '#A9A9A9',
-    marginLeft: 10,
-    marginRight: 10,
-  }
-});
+    container: {
+        flex: 1,
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    titleText: {
+        fontSize: 35,
+        fontWeight: "bold",
+        color: "white",
+        justifyContent: "center",
+        alignContent: "center",
+    },
+    contentCard: {
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 20,
+        elevation: 10,
+        backgroundColor: 'white'
+    },
+    itemImage: {
+        width: "100%",
+        height: 200,
+        marginBottom: 15,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 5
+    }
+})
